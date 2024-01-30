7b3ea142140747c371beeacb7ed0c34a6f97f6fe
Finn <finn@outlawz.nl>
2024-01-30 11:21:08 +0100

    Added 2 users to seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Enums\Association;
 use App\Models\MatchRequest;
 use App\Models\Power;
 use App\Models\Statistic;
@@ -12,6 +13,22 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
+        User::factory()->create([
+            'legal_name' => 'Hero Hero',
+            'alias' => 'Hero',
+            'email' => 'hero@hero.com',
+            'password' => 'hero',
+            'association' => Association::Hero,
+        ]);
+
+        User::factory()->create([
+            'legal_name' => 'Monster Monster',
+            'alias' => 'Monster',
+            'email' => 'monster@monster.com',
+            'password' => 'monster',
+            'association' => Association::Monster,
+        ]);
+
         $users = User::factory(10)->create();
 
         $users->each(function ($user) {
