4094678c6e4dce508a8e1af0efa773f68241410e
Finn <finn@outlawz.nl>
2024-01-30 10:46:07 +0100

    Removed some comments

--- app/Http/Requests/FormRequest.php
+++ app/Http/Requests/FormRequest.php
@@ -3,13 +3,14 @@
 namespace App\Http\Requests;
 
 use Exception;
+use Illuminate\Contracts\Validation\Validator;
 
 class FormRequest extends \Illuminate\Foundation\Http\FormRequest
 {
     /**
      * @throws Exception
      */
-    protected function failedValidation(\Illuminate\Contracts\Validation\Validator $validator)
+    protected function failedValidation(Validator $validator)
     {
         throw new Exception($validator->errors());
     }
