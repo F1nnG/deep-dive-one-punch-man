fafd8856bd36bd90cace0a3091ab727e4c4d0fc6
Lietze Diderich <lietze14@gmail.com>
2024-01-31 21:39:09 +0100

    minor fix

--- app/Filament/Resources/ApiKeyResource/Pages/ListApiKeys.php
+++ app/Filament/Resources/ApiKeyResource/Pages/ListApiKeys.php
@@ -8,9 +8,4 @@ use Filament\Resources\Pages\ListRecords;
 class ListApiKeys extends ListRecords
 {
     protected static string $resource = ApiKeyResource::class;
-
-    public static function getNavigationItemActiveRoutePattern(): string
-    {
-        return 'filament/association/resources/api-keys/index'; // TODO doesnt work yet (method never hit)
-    }
 }
